2997bf1fe4be78ebc541d576c80d040fe1cf6597
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-05-13 22:40:58 +0300

    Footer'a sosyal iconlar eklendi.

--- application/views/catalog/theme/footer.php
+++ application/views/catalog/theme/footer.php
@@ -5,7 +5,7 @@
 	    <div class="col-lg-8 col-md-10 mx-auto">
 		<ul class="list-inline text-center">
 		    <li class="list-inline-item">
-			<a href="https://github.com/zeynelakcay">
+			<a href="https://github.com/zeynelakcay" target="_blank">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
 				<i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
 			</a>
 		    </li>
 		    <li class="list-inline-item">
-			<a href="https://www.linkedin.com/in/zeynel-ak%C3%A7ay-8b696083/">
+			<a href="https://www.linkedin.com/in/zeynel-ak%C3%A7ay-8b696083/" target="_blank">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
 				<i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
@@ -21,7 +21,7 @@
 			</a>
 		    </li>
 		    <li class="list-inline-item">
-			<a href="https://twitter.com/zeynel_akcay">
+			<a href="https://twitter.com/zeynel_akcay" target="_blank">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
 				<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
@@ -29,7 +29,7 @@
 			</a>
 		    </li>
 		    <li class="list-inline-item">
-			<a href="https://www.instagram.com/zeynelakcay/">
+			<a href="https://www.instagram.com/zeynelakcay/" target="_blank">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
 				<i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
@@ -37,7 +37,7 @@
 			</a>
 		    </li>
 		    <li class="list-inline-item">
-			<a href="https://www.facebook.com/zeynelakcay61">
+			<a href="https://www.facebook.com/zeynelakcay61" target="_blank">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
 				<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
