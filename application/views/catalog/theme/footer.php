110148b2ae5a18fa0b5e2e7ab1831b0947824e98
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-05-13 22:10:08 +0300

    Footer'a sosyal iconlar eklendi.

--- application/views/catalog/theme/footer.php
+++ application/views/catalog/theme/footer.php
@@ -5,6 +5,22 @@
 	    <div class="col-lg-8 col-md-10 mx-auto">
 		<ul class="list-inline text-center">
 		    <li class="list-inline-item">
+			<a href="https://github.com/zeynelakcay">
+			    <span class="fa-stack fa-lg">
+				<i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-github fa-stack-1x fa-inverse"></i>
+			    </span>
+			</a>
+		    </li>
+		    <li class="list-inline-item">
+			<a href="#">
+			    <span class="fa-stack fa-lg">
+				<i class="fa fa-circle fa-stack-2x"></i>
+				<i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+			    </span>
+			</a>
+		    </li>
+		    <li class="list-inline-item">
 			<a href="#">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
@@ -16,7 +32,7 @@
 			<a href="#">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
-				<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+				<i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
 			    </span>
 			</a>
 		    </li>
@@ -24,7 +40,7 @@
 			<a href="#">
 			    <span class="fa-stack fa-lg">
 				<i class="fa fa-circle fa-stack-2x"></i>
-				<i class="fa fa-github fa-stack-1x fa-inverse"></i>
+				<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
 			    </span>
 			</a>
 		    </li>
